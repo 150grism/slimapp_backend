7f349d68c6ff7e7dcde0e87b148e78b0e13ddb22
Grigory Moshkevich <ilcostuff@gmail.com>
2017-12-27 04:44:55 +0300

    added saving breed to breeds table and usersavedbreeds table with post request

--- src/routes/users.php
+++ src/routes/users.php
@@ -18,7 +18,6 @@ $app->add(function ($req, $res, $next) {
 });
 
 //Get all users
-
 $app->get('/api/users', function(Request $request, Response $response) {
   $sql = "SELECT * FROM users";
   
@@ -35,5 +34,82 @@ $app->get('/api/users', function(Request $request, Response $response) {
 
   } catch(PDOException $e) {
     echo '{"error": {"text": ' . $e->getMessage() . '}';
+  }
+});
+
+//Get usersavedbreeds
+$app->get('/api/usersavedbreeds', function(Request $request, Response $response) {
+  $sql = "SELECT * FROM usersavedbreeds";
+  
+  try {
+    //Get DB object
+    $db = new db();
+    //Connect
+    $db = $db->connect();
+
+    $stmt = $db->query($sql);
+    $usersavedbreeds = $stmt->fetchALL(PDO::FETCH_OBJ);
+    $db = null;
+    echo json_encode($usersavedbreeds);
+
+  } catch(PDOException $e) {
+    echo '{"error": {"text": ' . $e->getMessage() . '}';
+  }
+});
+
+//Get saved breeds for a user
+$app->get('/api/user/{id}', function(Request $request, Response $response) {
+  $id = $request->getAttribute('id');
+  $sql = "SELECT b.breed_name FROM usersavedbreeds AS ub INNER JOIN breeds AS b ON ub.breed_id = b.breed_id";
+  
+  try {
+    //Get DB object
+    $db = new db();
+    //Connect
+    $db = $db->connect();
+
+    $stmt = $db->query($sql);
+    $usersavedbreeds = $stmt->fetchALL(PDO::FETCH_OBJ);
+    $db = null;
+    echo json_encode($usersavedbreeds);
+
+  } catch(PDOException $e) {
+    echo '{"error": {"text": ' . $e->getMessage() . '}';
+  }
+});
+
+//Save breed for a user
+$app->post('/api/users/{id}/save', function(Request $request, Response $response) {
+  $id = $request->getAttribute('id');
+  $breed = $request->getParam('breed');
+  $sql1 = "INSERT INTO breeds (breed_name) VALUES (:breed)";
+  $sql2 = "INSERT INTO usersavedbreeds (user_id, breed_id) SELECT :id, breed_id FROM breeds WHERE breed_name=:breed";
+  
+  try {
+    //Get DB object
+    $db = new db();
+    //Connect
+    $db = $db->connect();
+
+    //1
+    $stmt = $db->prepare($sql1);
+    
+    // $stmt->bindParam(':id', $id);
+    $stmt->bindParam(':breed', $breed);
+
+    $stmt->execute();
+
+    //2
+    $stmt = $db->prepare($sql2);
+    
+    $stmt->bindParam(':id', $id);
+    $stmt->bindParam(':breed', $breed);
+
+    $stmt->execute();
+    $db = null;
+
+    echo '{"notice": {"text": "Breed added"}}';
+  } catch(PDOException $e) {
+    echo '{"error": {"text": ' . $e->getMessage() . '}}';
   }
 });
